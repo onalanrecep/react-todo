a4849a5e83090da5f00be3a83883dfd7a93187b6
Recep Önalan <onalanrecep86@gmail.com>
2020-07-03 23:28:58 +0300

    baslik pointer ve Gorev state ve toggle event eklendi

--- todoreact/src/components/Gorev.js
+++ todoreact/src/components/Gorev.js
@@ -2,18 +2,39 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 class Gorev extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isVisible : false
+        }
+    }
+
+    onClickEvent = (number, e) => {
+        this.setState({
+            isVisible : !this.state.isVisible
+        })
+    }
+
     render() {
         //Destructing
         const {baslik,tanim,sorumlu,durum,oncelik} = this.props;
+        const {isVisible} = this.state;
         return (  
         <div className="gorev-card">
-            <h4 className="gor-bas">{baslik}</h4>
-            <p>{tanim}</p>
-            <div className="gor-durumlar">
-              <div className="durumlar">{sorumlu}</div>
-              <div className="durumlar">{durum}</div>
-              <div className="durumlar">{oncelik}</div>
-            </div>
+            <h4 className="gor-bas" onClick = {this.onClickEvent.bind(this)}>{baslik}</h4>
+            {
+                isVisible ? <div>
+                <p>{tanim}</p>
+                <div className="gor-durumlar">
+                <div className="durumlar">{sorumlu}</div>
+                <div className="durumlar">{durum}</div>
+                <div className="durumlar">{oncelik}</div>
+                </div>
+            </div> : null
+            }
+
         </div>    
         )
     }
