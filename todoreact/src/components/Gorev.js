a6d33d52adca5a3c7f6c9527f3f9e38f337738d7
Recep Önalan <onalanrecep86@gmail.com>
2020-07-03 22:17:22 +0300

    Propstype eklendi

--- todoreact/src/components/Gorev.js
+++ todoreact/src/components/Gorev.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class Gorev extends Component {
     render() {
@@ -16,6 +17,21 @@ class Gorev extends Component {
         </div>    
         )
     }
+}
+Gorev.defaultProps = {
+    baslik : "Belirtilmedi",
+    tanim : "Belirtilmedi",
+    sorumlu : "Belirtilmedi",
+    durum : "Belirtilmedi",
+    oncelik : "Belirtilmedi"
+}
+
+Gorev.propTypes = {
+    baslik : PropTypes.string.isRequired,
+    tanim : PropTypes.string.isRequired,
+    sorumlu : PropTypes.string.isRequired,
+    durum : PropTypes.string.isRequired,
+    oncelik : PropTypes.string.isRequired
 }
 
 export default Gorev;
